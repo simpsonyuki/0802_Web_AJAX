137dbef225f9afdbdb409f87ca254db208635f91
simpsonyuki <yukilin0327@gmail.com>
2020-08-02 10:41:40 +0800

    ajax指令

--- ajax.js
+++ ajax.js
@@ -1,1 +1,7 @@
 var content = document.getElementById("content");
+
+var xhr = new XMLHttpRequest();   //跟後端伺服器要資料的物件指令
+
+xhr.open("GET", "./ajax.js", "true");     //開啟物件("取得", "檔案路徑", "要非同步處理")
+                                          //這裡最後面的布林值預設就已經是"true"了,所以也可以不用寫
+xhr.send();                       //傳送
